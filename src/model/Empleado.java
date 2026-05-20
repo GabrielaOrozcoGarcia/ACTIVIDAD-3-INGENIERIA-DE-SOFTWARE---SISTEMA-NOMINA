92a12aad7f8ef071bdee964ee840b9c64218cc3a
BCpollito <victo.elcatire75@gmail.com>
2026-05-20 15:46:26 -0500

    implementando la interfaz Calculable en la clase abstracta Empleado para asegurar el diseño basado en contratos

--- src/model/Empleado.java
+++ src/model/Empleado.java
@@ -1,25 +1,21 @@
 package model;
-
-public abstract class Empleado {
+import interfaces.Calculable;
+/**
+ * Clase abstracta base que representa la estructura general de un Empleado.
+ * Implementa Calculable para asegurar el diseño basado en contratos.
+ */
+public abstract class Empleado implements Calculable {
 
     protected String nombre;
     protected String identificacion;
     protected double salarioBase;
 
-    // Constructor para inicializar los datos comunes
     public Empleado(String nombre, String identificacion, double salarioBase) {
         this.nombre = nombre;
         this.identificacion = identificacion;
         this.salarioBase = salarioBase;
     }
 
-    /**
-     * Método abstracto que cada subtipo de empleado debe implementar
-     * para calcular su salario bruto según sus propias reglas de negocio.
-     */
-    public abstract double calcularSalario();
-
-    // Getters necesarios para el reporte final de nómina
     public String getNombre() { return nombre; }
     public String getIdentificacion() { return identificacion; }
     public double getSalarioBase() { return salarioBase; }
